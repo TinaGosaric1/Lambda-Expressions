1d5119fb13a2651b772529a58e665d3c249dc5cb
Tina Gosarič <tina.gosaric@gorenje.com>
2022-11-25 09:35:41 +0100

    Implementing a thread with lambda expression

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // without lambda expression
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Printing from the Runnable1");
+            }
+        }).start();
+
+        // with lambda expression
+
+        new Thread(()-> {
+            System.out.println("Printing from the Runnable2");
+            System.out.println("One more statement");
+        }).start();
     }
 }
