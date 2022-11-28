49850aeb88adc859bc6a4628ae2bbdf105b5154e
Tina Gosarič <tina.gosaric@gorenje.com>
2022-11-28 12:08:42 +0100

    Interface + lambda expression

--- src/Main.java
+++ src/Main.java
@@ -49,6 +49,27 @@ public class Main {
         for (Employee employee : employees) {
             System.out.println(employee.getName());
         }
+
+        // without lambda expression
+
+//        String sillyString = doStringStuff(new UpperConcat() {
+//            @Override
+//            public String upperAndConcat(String s1, String s2) {
+//                return s1.toUpperCase() + s2.toUpperCase();
+//            }
+//        },
+//                employees.get(0).getName(), employees.get(1).getName());
+//        System.out.println(sillyString);
+
+        // with lambda expression
+
+        UpperConcat uc = (s1, s2) -> s1.toUpperCase() + s2.toUpperCase();
+        String sillyString = doStringStuff(uc, employees.get(0).getName(), employees.get(1).getName());
+        System.out.println(sillyString);
+    }
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1, s2);
     }
 }
 
@@ -76,4 +97,8 @@ class Employee {
     public void setAge(int age) {
         this.age = age;
     }
+}
+
+interface UpperConcat {
+    public String upperAndConcat(String s1, String s2);
 }
