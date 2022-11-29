90e2904065ee4c22c4f536a339fb39e47d5e217c
Tina Gosarič <tina.gosaric@gorenje.com>
2022-11-29 14:42:14 +0100

    Lambda + forEach

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,8 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-        // without lambda expression
-
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                System.out.println("Printing from the Runnable1");
-//            }
-//        }).start();
-
-        // with lambda expression
-
-        new Thread(() -> {
-            System.out.println("Printing from the Runnable2");
-            System.out.println("One more statement");
-        }).start();
-
         Employee eli = new Employee("Eli Kaloh", 5);
         Employee joel = new Employee("Joel Berghult", 36);
         Employee iza = new Employee("Iza Audrey", 21);
@@ -33,75 +14,9 @@ public class Main {
         employees.add(iza);
         employees.add(anne);
 
-        // without lambda expression
-
-//        Collections.sort(employees, new Comparator<Employee>() {
-//            @Override
-//            public int compare(Employee employee1, Employee employee2) {
-//                return employee1.getName().compareTo(employee2.getName());
-//            }
-//        });
-
-        // with lambda expression
-
-        Collections.sort(employees, (employee1, employee2) -> employee1.getName().compareTo(employee2.getName()));
-
-        for (Employee employee : employees) {
+        employees.forEach(employee -> {
             System.out.println(employee.getName());
-        }
-
-        // without lambda expression
-
-//        String sillyString = doStringStuff(new UpperConcat() {
-//            @Override
-//            public String upperAndConcat(String s1, String s2) {
-//                return s1.toUpperCase() + s2.toUpperCase();
-//            }
-//        },
-//                employees.get(0).getName(), employees.get(1).getName());
-//        System.out.println(sillyString);
-
-        // with lambda expression
-
-        UpperConcat uc = (s1, s2) -> {
-            String result = s1.toUpperCase() + s2.toUpperCase(); // assign lambda to variable
-            return result;
-        };
-        String sillyString = doStringStuff(uc, employees.get(0).getName(), employees.get(1).getName());
-        System.out.println(sillyString);
+            System.out.println(employee.getAge());
+        });
     }
-
-    public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
-        return uc.upperAndConcat(s1, s2);
-    }
-}
-
-class Employee {
-    private String name;
-    private int age;
-
-    public Employee(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-}
-
-interface UpperConcat {
-    public String upperAndConcat(String s1, String s2);
 }
